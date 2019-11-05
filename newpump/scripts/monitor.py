108509db6a5abc1fa8976ee96f3202fec8e4f99f
mcxiaoke <mcxiaoke@live.com>
2019-11-05 14:16:24 +0800

    newpump: add monitor.py

--- newpump/scripts/monitor.py
+++ newpump/scripts/monitor.py
@@ -1,34 +1,94 @@
+from datetime import datetime
+import logging
+import requests
+import re
 import paho.mqtt.client as mqtt
 from config import *
 
-# The callback for when the client receives a CONNACK response from the server.
+def get_full_class_name(obj):
+    module = obj.__class__.__module__
+    if module is None or module == str.__class__.__module__:
+        return obj.__class__.__name__
+    return module + '.' + obj.__class__.__name__
+
+def get_log_filename():
+    dt = datetime.now().strftime("%Y%m")
+    return '/tmp/pump-monitor-{}.log'.format(dt)
 
 
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
+def logging_config():
+    logging.basicConfig(level=logging.INFO,
+                        format='[%(asctime)s][%(name)s][%(levelname)s] %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S',
+                        filename=get_log_filename(),
+                        filemode='a')
+    console = logging.StreamHandler()
+    console.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '[%(asctime)s][%(name)s][%(levelname)s] %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
 
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
-    client.subscribe("#")
 
-# The callback for when a PUBLISH message is received from the server.
+logging_config()
+logger = logging.getLogger("monitor")
+
+
+def send_report(sender, msg):
+    dtStr = datetime.now().strftime("%m%d%H%M%S")
+    try:
+        data = {
+            "text": "Pump_Status_{}_{}".format(sender, dtStr),
+            "desp": msg.replace("\n", "  \n"),
+        }
+        r = requests.post(WX_REPORT_URL, data=data, timeout=5)
+        # print(r.text)
+        if r.ok:
+            logger.info("Send %s report successful", sender)
+        else:
+
+            logger.warning("Send %s report failed, status: %s",
+                           sender, r.status_code)
+    except Exception as e:
+        logger.warning("Send %s report failed: %s", sender,  get_full_class_name(e))
 
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
-    if(msg.topic.startswith("pump/status")):
-        print("Pump status message received!")
+    topic = msg.topic
+    message = msg.payload.decode('utf8')
+    logger.info(topic+" - "+message.replace("\n", " ") +
+                " ("+str(msg.qos)+","+str(msg.retain)+")")
+    m = re.match(r"^pump/(\S+)/status$", topic)
+    if m and m.group(1):
+        send_report(m.group(1), message)
 
 
-client = mqtt.Client(MQTT_CLIENT_ID)
-client.on_connect = on_connect
-client.on_message = on_message
-client.username_pw_set(MQTT_USER, MQTT_PASS)
-client.connect(MQTT_SERVER, port=MQTT_PORT, keepalive=60)
+def on_connect(client, userdata, flags, rc):
+    logger.info("Connected with result: "+mqtt.error_string(rc))
+    # client.subscribe("$SYS/#")
+    client.subscribe("#")
+    client.publish("monitor/pump", "Online", retain=True)
 
-# Blocking call that processes network traffic, dispatches callbacks and
-# handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+
+def on_disconnect(client, userdata, rc):
+    logger.info("Disconnected with result: "+mqtt.error_string(rc))
+
+
+def create_client():
+    client = mqtt.Client(client_id=MQTT_CLIENT_ID, clean_session=True)
+    # client.enable_logger()
+    # client.on_log = on_log
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    client.on_message = on_message
+    client.username_pw_set(MQTT_USER, MQTT_PASS)
+    client.will_set("monitor/pump", payload="Offline", retain=True)
+    client.connect(MQTT_SERVER, port=MQTT_PORT, keepalive=60)
+    return client
+
+
+if __name__ == "__main__":
+    client = create_client()
+    logger.info("====================")
+    logger.info("Pump monitor start")
+    client.loop_forever()
