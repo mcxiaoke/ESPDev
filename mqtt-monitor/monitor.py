ee1489682bcd45bdfc8dd29421f4835fea6337d1
mcxiaoke <mcxiaoke@live.com>
2021-03-21 12:35:53 +0800

    update mqtt monitor script

--- mqtt-monitor/monitor.py
+++ mqtt-monitor/monitor.py
@@ -3,18 +3,29 @@
 from datetime import datetime
 import logging
 import requests
+import threading
 import re
 import time
 import paho.mqtt.client as mqtt
 from config import *
 
-MSG_LIMIT_PER_MIN = 20
-MSG_LIMIT_PER_HOUR = 50
-MSG_LIMIT_PER_DAY = 800
+MSG_LIMIT_PER_MIN = 60
+MSG_LIMIT_PER_HOUR = 300
+MSG_LIMIT_PER_DAY = 1500
 
 sendCounter = {}
 titleCounter = 0
 
+IP = '127.0.0.1'
+
+def get_public_ip():
+    global IP
+    try:
+        json = requests.get('http://ip-api.com/json').json()
+        IP =  json['query']
+        logger.info('Public IP:', IP)
+    except Exception as e:
+        logger.error(e)
 
 def get_full_class_name(obj):
     module = obj.__class__.__module__
@@ -48,12 +59,15 @@ logger = logging.getLogger("monitor")
 
 def send_ios_report(data):
     try:
-        url = BARK_REPORT_URL.format(data['text'], data['desp'])
+        url = BARK_REPORT_URL.format(data['title'], data['desp'])
         r = requests.post(url, timeout=10)
         if r.ok:
-            logger.debug("iOS Sent: %s", data["text"])
-    except Exception as e:
-        logger.warning("iOS Send report failed: %s", get_full_class_name(e))
+            logger.debug("iOS sent: %s", data["title"])
+        else:
+            logger.warning("iOS send for %s failed, error: %s %s",
+                           sender, r.status_code, r.text)
+    except Exception:
+        logger.exception('ios send failed')
 
 
 def send_report(sender, msg):
@@ -75,66 +89,72 @@ def send_report(sender, msg):
     sendCounter[min_key] = min_value + 1
     sendCounter[day_key] = day_value + 1
     titleCounter += 1
-    suffix = now.strftime("%Y%m%d")
-    msg += '\nCreated At '
-    msg += now.strftime("%Y-%m-%d %H:%M:%S")
     data = {
-        "text": "Device_{}_{}_{}".format(sender, suffix, titleCounter),
-        "desp": msg.replace("\n", "  \n"),
+        "title": "Device_{}_Message_{}".format(sender, titleCounter),
+        "desp": msg,
     }
     try:
-        r = requests.post(WX_REPORT_URL, data=data, timeout=10)
-        if r.ok and 'pushid' in r.text:
-            logger.info("Sent: [%s]", data["text"])
-            logger.debug("Send report to %s successful", sender)
+        r = requests.post(WX_REPORT_URL, params=data, timeout=10)
+        if r.ok:
+            logger.info("Sent: [%s]", data["title"])
+            logger.debug("Send report for %s successful", sender)
         else:
-
-            logger.warning("Send report to %s failed, status: %s",
-                           sender, r.status_code)
-    except Exception as e:
-        logger.warning("Send report to %s failed: %s",
-                       sender, get_full_class_name(e))
+            logger.warning("Send report for %s failed, error: %s %s",
+                           sender, r.status_code, r.text)
+    except Exception:
+        logger.exception('send report failed')
     time.sleep(1)
     send_ios_report(data)
 
-
+def send_online():
+    time.sleep(1)
+    logger.info('Send monitor online')
+    client.publish("device/online", "MQTT Monitor/Online {}".format(IP), retain=True)
+    send_report("monitor", "MQTT Monitor/Online {}".format(IP))
+    
 def on_message(client, userdata, msg):
     topic = msg.topic
     message = msg.payload.decode('utf8')
     logMsg = "[{}]:<{}> ({},{})".format(topic, message, msg.qos, msg.retain)
-    logger.debug(logMsg)
+    logger.info(logMsg)
     if topic == 'device/monitor/status':
+        return
+    if topic == 'device/check':
+        t = threading.Thread(target=send_online)
+        t.start()
         return
     m = re.match(r"^device/(\S+)/status$", topic)
     if m and m.group(1):
-        send_report(m.group(1), message)
+        t = threading.Thread(target=send_report, args= (m.group(1), message,))
+        t.start()
+#         send_report(m.group(1), message)
 
 
 def on_connect(client, userdata, flags, rc):
     logger.info("Connected with result: " + mqtt.error_string(rc))
     # client.subscribe("$SYS/#")
-    client.subscribe("#")
-    client.publish("device/monitor/status", "Online", retain=True)
-
+    client.subscribe("device/#")
+    _thread.start_new_thread(send_online)
 
 def on_disconnect(client, userdata, rc):
     logger.info("Disconnected with result: " + mqtt.error_string(rc))
 
 
 def create_client():
-    client = mqtt.Client(client_id=MQTT_CLIENT_ID, clean_session=True)
+    client = mqtt.Client(client_id=MQTT_CLIENT_ID, clean_session=False)
     # client.enable_logger()
     # client.on_log = on_log
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = on_message
     client.username_pw_set(MQTT_USER, MQTT_PASS)
-    client.will_set("device/monitor/status", payload="Offline", retain=True)
+    client.will_set("device/online", payload="MQTT Monitor/Offline {}".format(IP), retain=True)
     client.connect(MQTT_SERVER, port=MQTT_PORT, keepalive=60)
     return client
 
 
 if __name__ == "__main__":
+    get_public_ip()
     client = create_client()
     logger.info("====================")
     logger.info("MQTT Monitor Started")
